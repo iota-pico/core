cbca0824a6b395729f8189d8529329ba92c1e87e
Martyn Janes <martyn@obany.com>
2018-01-31 11:44:55 +0300

    Removed ILogger

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ export * from "./helpers/errorHelper";
 export * from "./helpers/jsonHelper";
 export * from "./helpers/stringHelper";
 export * from "./interfaces/IError";
-export * from "./interfaces/ILogger";
 export * from "./interfaces/INetworkClient";
 export * from "./interfaces/INetworkEndPoint";
 export * from "./interfaces/networkProtocol";
